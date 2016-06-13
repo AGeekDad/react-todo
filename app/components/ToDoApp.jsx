9bf3de1022dc9100ff2c4ec40fc78acfc8333e9b
swood <swood@fanatic.com>
2016-06-13 10:49:08 -0500

    Basic components with tests.

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,14 +1,25 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
-var Main = React.createClass({
+var ToDoApp = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: [
+        { id: 1, text: 'code app' },
+        { id: 2, text: 'test app' },
+        { id: 4, text: '3rd' }
+      ]
+    };
+  },
   render: function() {
+    var { todos } = this.state;
     return (
       <div>
-            <p>ToDo Rendered</p>
-            {props.children}
+            <p>ToDoApp Rendered</p>
+            <TodoList todos={todos} />
       </div>
     );
   }
 });
 
-module.exports = Main;
+module.exports = ToDoApp;
