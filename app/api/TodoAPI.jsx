46ffa9a1f66de9f7e1a9099e372274546f550511
swood <swood@fanatic.com>
2016-06-13 16:56:59 -0500

    search and sort

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,23 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText){
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    var searchTxt = searchText.toLowerCase();
+    filteredTodos = filteredTodos.filter((todo) => {
+      return searchText.length === 0 || todo.text.toLowerCase().includes(searchTxt);
+    });
+
+    filteredTodos.sort((a,b) => {
+      return a.completed == b.completed ? 0 : (a.completed ? 1 : -1);
+    });
+
+    return filteredTodos;
   }
 };
--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -40,12 +40,13 @@ var ToDoApp = React.createClass({
     });
   },
   render: function() {
-    var { todos } = this.state;
+    var { todos, showCompleted, searchText } = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
     return (
       <div>
         <p>ToDoApp Rendered</p>
         <TodoSearch onSearch={this.handleSearch} />
-        <TodoList todos={todos} onToggle={this.handleToggle} />
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle} />
         <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     );
