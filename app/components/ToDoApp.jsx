3ac221376c7501a090d6517f188e3f24558458fc
swood <swood@fanatic.com>
2016-06-13 11:16:05 -0500

    Stub search

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -2,10 +2,13 @@ var React = require('react');
 
 var AddTodo = require('AddTodo');
 var TodoList = require('TodoList');
+var TodoSearch = require('TodoSearch');
 
 var ToDoApp = React.createClass({
   getInitialState: function(){
     return {
+      showCompleted: false,
+      searchText: '',
       todos: [
         { id: 1, text: 'code app' },
         { id: 2, text: 'test app' },
@@ -16,11 +19,18 @@ var ToDoApp = React.createClass({
   handleAddTodo: function(text) {
     alert('new todo: ' + text);
   },
+  handleSearch: function(showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function() {
     var { todos } = this.state;
     return (
       <div>
             <p>ToDoApp Rendered</p>
+            <TodoSearch onSearch={this.handleSearch} />
             <TodoList todos={todos} />
             <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
