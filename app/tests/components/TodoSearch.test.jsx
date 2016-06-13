c664a1d8abfd1f87f454b3b5acb906b2c1f3c665
swood <swood@fanatic.com>
2016-06-13 11:26:41 -0500

    search tests

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,38 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoSearch = require('TodoSearch');
+
+describe('TodoSearch', () => {
+  it('should exist', () => {
+    expect(TodoSearch).toExist();
+  });
+
+  it('Search text', () => {
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    var $el = $(ReactDOM.findDOMNode(todoSearch));
+
+    todoSearch.refs.searchText.value = 'search';
+    TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+    expect(spy).toHaveBeenCalledWith(false, 'search');
+  });
+
+  it('Search text blank, do not show completed', () => {
+    var spy = expect.createSpy();
+    var todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy}/>);
+
+    var $el = $(ReactDOM.findDOMNode(todoSearch));
+
+    todoSearch.refs.showCompleted.checked = true;
+    TestUtils.Simulate.change(todoSearch.refs.showCompleted);
+
+    expect(spy).toHaveBeenCalledWith(true, '');
+  });
+
+});
