dcdbd36ddcd77eec5c8496fd70e1fb9b2d58d708
swood <swood@fanatic.com>
2016-06-16 12:30:51 -0500

    test add todo firebase

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -1,6 +1,10 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 var expect = require('expect');
 
 var actions = require('actions');
+
+var createMockStore = configureMockStore([thunk]);
 
 describe('Actions', () => {
   it('should generate search text action', () => {
@@ -25,11 +29,33 @@ describe('Actions', () => {
   it('should generate add todo action', () => {
     var action = {
       type: 'ADD_TODO',
-      text: 'Thing to do'
+      todo: {
+          id: '123',
+          text: 'Walk the dog',
+          completed: false,
+          createAt: 12343214
+      }
     };
-    var res = actions.addTodo(action.text);
+    var res = actions.addTodo(action.todo);
 
     expect(res).toEqual(action);
+  });
+
+  it('should create todo and dispatch addtodo', (done) => {
+    const store = createMockStore({});
+    const todoText = 'todo item';
+
+    store.dispatch(actions.startAddTodo(todoText)).then(() => {
+        const actions = store.getActions();
+
+        expect(actions[0]).toInclude({
+          type: 'ADD_TODO'
+        });
+        expect(actions[0].todo).toInclude({
+          text: todoText
+        });
+        done();
+    }).catch(done());
   });
 
   it('should generate toggle todo action', () => {
--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -5,6 +5,7 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var { AddTodo } = require('AddTodo');
+import * as actions from 'actions';
 
 describe('AddTodo', () => {
   it('should exist', () => {
@@ -12,18 +13,17 @@ describe('AddTodo', () => {
   });
 
   it('new todo is added', () => {
+    var todoText = 'new todo';
+    var action = actions.startAddTodo(todoText);
     var spy = expect.createSpy();
     var addTodo = TestUtils.renderIntoDocument(<AddTodo dispatch={spy}/>);
 
     var $el = $(ReactDOM.findDOMNode(addTodo));
 
-    addTodo.refs.todoText.value = 'new todo';
+    addTodo.refs.todoText.value = todoText;
     TestUtils.Simulate.submit($el.find('form')[0]);
 
-    expect(spy).toHaveBeenCalledWith({
-        type: 'ADD_TODO',
-        text: 'new todo'
-    });
+    expect(spy).toHaveBeenCalledWith(action);
   });
 
   it('blank text with button click', () => {
