7fb492723a856fabc55c2865f038aa2b9a95d450
swood <swood@fanatic.com>
2016-06-16 15:12:41 -0500

    move firebase cred to ENV

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,10 +3,10 @@ import firebase from 'firebase';
 try {
 
   var config = {
-    apiKey: "AIzaSyAAwpc7LPIggYUgK6TbJjltRXGNwWPpYTw",
-    authDomain: "react-todo-60db2.firebaseapp.com",
-    databaseURL: "https://react-todo-60db2.firebaseio.com",
-    storageBucket: "react-todo-60db2.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
 
   firebase.initializeApp(config);
