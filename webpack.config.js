a2366b4ec847cadbeae042aa0f1e9ded29f43078
swood <swood@fanatic.com>
2016-06-17 11:15:02 -0500

    debug on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -76,5 +76,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production1' ? undefined : 'cheap-module-eval-source-map'
 };
